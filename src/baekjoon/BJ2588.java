6b03e9d83a717f8b40c37fa0718e5e50c312b4e8
apffhel <xxrosevly@gmail.com>
2025-01-26 23:44:02 +0900

    2025.01.26

---
+++ src/baekjoon/BJ2588.java
@@ -0,0 +1,19 @@
+package baekjoon;
+
+import java.util.Scanner;
+
+public class BJ2588 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        int num1 = scan.nextInt();
+        int num2 = scan.nextInt();
+
+        int num3 = num1;
+
+        // 이어서
+
+
+    }
+
+}
